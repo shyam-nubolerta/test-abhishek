1e5b791e85647d705dd92ee0b32f791ec8eeff9e
tiwabhishek2024 <tiwabhishek@gmail.com>
2024-09-13 15:44:29 +0530

    fixing issues

--- src/test/java/com/nubolerta/tech/users/UsersApplicationTests.java
+++ src/test/java/com/nubolerta/tech/users/UsersApplicationTests.java
@@ -1,13 +1,7 @@
 package com.nubolerta.tech.users;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UsersApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
